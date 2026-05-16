265570b616fb88e683d30549082e1f967e7c8dca
nhandeptraiii <thanhnhan21082003@gmail.com>
2026-05-16 12:38:09 +0700

    feat: initial commit

---
+++ src/main/java/vn/viettel/khdn/billing_platform/BillingPlatformApplication.java
@@ -0,0 +1,13 @@
+package vn.viettel.khdn.billing_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/viettel/khdn/billing_platform/BillingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.viettel.khdn.billing_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
